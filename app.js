78b988e9b341861b3c64b7c56e925b46dead8c82
iamadityaraj04 <adityarohitraj@gmail.com>
2022-09-08 20:44:42 +0530

    bi-Assignment

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express=require("express")
 const app=express();
 
+//requiring db
+require("./db/dbConnect.js");
 const port=8080;
 
 app.get('/',(req,res)=>{
     res.send("Board Infinity Assignment");
 })
+
+//starting server
 app.listen(port,()=>{
     console.log(`App is running on ${port}...`);
 })
